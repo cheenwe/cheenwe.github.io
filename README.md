b6f43fc9b3ad40ad770a2c60428b1c5bd0e1765b
chenwei <cheenwe@gmail.com>
2016-10-20 09:25:33 +0800

    feat(remove disqus):

--- README.md
+++ README.md
@@ -22,15 +22,7 @@ swiftype官网： <https://swiftype.com/>.
 3. 找到`swiftype_searchId`.
 
 
-## 2.disqus
-
-开启 disqus 评论功能.
-
-disqus官网： <https://duoshuo.com/>.
-
-修改 `disqus_shortname` 在 `_config.yml`.
-
-## 3. 多说
+## 2. 多说
 
 开启评论功能.
 
--- static/js/post.js
+++ static/js/post.js
@@ -29,15 +29,3 @@ function share(){
 
 }
 
-
-function disqus(){
-    /* * * CONFIGURATION VARIABLES * * */
-    var disqus_shortname = '{{site.disqus_shortname}}';
-
-    var dsq = document.createElement('script');
-    dsq.type = 'text/javascript';
-    dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0]
-     || document.getElementsByTagName('body')[0]).appendChild(dsq);
-}
