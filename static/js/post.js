073f76d3402468fccd5a17c81ac4053bbe24883f
chenwei <cheenwe@gmail.com>
2016-10-20 09:16:03 +0800

    deving

--- static/js/post.js
+++ static/js/post.js
@@ -38,5 +38,5 @@ function disqus(){
     dsq.async = true;
     dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
     (document.getElementsByTagName('head')[0]
-     || document.getElementsByTagName('body')[0]).appendChild(ds);
+     || document.getElementsByTagName('body')[0]).appendChild(dsq);
 }
