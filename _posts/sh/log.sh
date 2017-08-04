fa8aae35398dc28b8884507528ee1a248ca6605e
chenwei <cheenwe@gmail.com>
2017-08-04 16:43:56 +0800

    change shell

--- _posts/sh/log.sh
+++ _posts/sh/log.sh
@@ -6,8 +6,8 @@
 #  version     : 0.0.1
 #  created     : 2017.8.04
 #  description : shell 日志文件输出打印
-#                  输出日志到终端显示颜色及输入日志到文件  
-#                   usage: bash log.sh &
+#                  输出日志到终端显示颜色及输入日志到文件
+#                   usage:  bash log.sh &
 #  history     : 日志文件
 #               1. Date: 2017.8.4
 #               Author:  cheenwe
@@ -23,11 +23,11 @@ YELLOW="\\033[33m"
 BLACK="\\033[0m"
 POS="\\033[60G"
 
-date=`date '+%Y%m%d'`
-now=`date '+%Y%m%d %H:%M:%S'`
+date=`date '+%Y-%m-%d'`
+now=`date '+%Y-%m-%d %H:%M:%S'`
 
 APP_NAME='log.sh'
-APP_PATH=`pwd` 
+APP_PATH=`pwd`
 APP_LOG_FILE="${APP_PATH}/log/${APP_NAME}.${date}.log"
 
 # get log file
@@ -45,7 +45,7 @@ if [ ! -d "$log_dir" ]; then
     ok_msg "create log( ${log} ) success"
     ok_msg "see detail log: tailf ${log}"
 fi
- 
+
 ok_msg() {
     echo -e "${now} ${1}${POS}${BLACK}[${GREEN}  OK  ${BLACK}]"
     echo "[${now}] [INFO] ${1}" >> $log
@@ -61,7 +61,7 @@ failed_msg() {
 }
 
 for ((i = 0; i < 50000; i++)); do
-    # sleep a little while 
+    # sleep a little while
     sleep 1
     warn_msg "${i} Myapp start warn";
     ok_msg "Myapp start ok";
